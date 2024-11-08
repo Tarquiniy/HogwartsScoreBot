5493c6d21063f5abd7ac8cc411ea33dee5928735
Hius <150480617+Tarquiniy@users.noreply.github.com>
2024-11-09 00:17:09 +0500

    Add protection from telegram network errors

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, exceptions
 from settings import settings
 import asyncio
 from handlers import router
@@ -11,17 +11,32 @@ async def start_bot(bot: Bot):
 async def stop_bot(bot: Bot):
     await bot.send_message(settings.bots.admin_id, text='Бот остановлен!')
 
+
 async def start():
     bot = Bot(token=settings.bots.bot_token, parse_mode='HTML')
     dp = Dispatcher()
     dp.include_router(router)
     dp.startup.register(start_bot)
     dp.shutdown.register(stop_bot)
-    await dp.start_polling(bot)
+    try:
+        await dp.start_polling(bot)
+    except exceptions.TelegramNetworkError:
+        await restart_bot(bot, dp)
+    except KeyboardInterrupt:
+        print('Exit')
+
+
+async def restart_bot(bot: Bot, dp: Dispatcher):
+    await bot.session.close()
+    await asyncio.sleep(1)
+    try:
+        await dp.start_polling(bot)
+    except exceptions.TelegramNetworkError:
+        await restart_bot(bot, dp)
 
 
 if __name__ == "__main__":
     try:
         asyncio.run(start())
     except KeyboardInterrupt:
-       print('Exit')
+        print('Exit')
